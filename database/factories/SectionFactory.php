8955e0f5f5dedb833c6231ee49275768fe860dc7
kual Louis <mubaraklouis@gmail.com>
2022-10-09 15:03:10 +0200

    working the section filter

---
+++ database/factories/SectionFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Students;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Section>
+ */
+class SectionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'student_id' => Students::factory(),
+            'Full_Marks' => $this->faker->numerify('###'),
+            'Index_Number'=>$this->faker->numerify("#####"),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Section;
 use Illuminate\Database\Seeder;
 use \App\Models\Students;
 
@@ -16,6 +18,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         Students::factory(10)->create();
+        Section::factory(2)->create();
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
